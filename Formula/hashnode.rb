3922e5d56cd0cc94a812bb33a3cc0a6c93e89831
goreleaserbot <goreleaser@hashnode.com>
2019-03-05 15:57:18 +0530

    Brew formula update for hashnode-cli version v0.1.12

--- Formula/hashnode.rb
+++ Formula/hashnode.rb
@@ -1,9 +1,9 @@
 class Hashnode < Formula
   desc "Get hashnode.com on your terminal"
   homepage "https://hashnode.com"
-  url "https://github.com/hashnode/hashnode-cli/releases/download/v0.1.11/hashnode-darwin-amd64.tar.gz"
-  version "0.1.11"
-  sha256 "fd273c1ccab771f7ae9850b76949a9bff32d0b3856b2cc8ef307d4bd7ff331f2"
+  url "https://github.com/hashnode/hashnode-cli/releases/download/v0.1.12/hashnode-darwin-amd64.tar.gz"
+  version "0.1.12"
+  sha256 "e74fc41b2a8e48321734364803674f5ae7d8edc9d6271438ba09488bca143905"
 
   def install
     bin.install "hashnode"
